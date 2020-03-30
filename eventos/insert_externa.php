f8848bf6ebd0f0f3f9e668ad898746ef76d83f8b
VinNeko-ZX <55595309+VinNeko-ZX@users.noreply.github.com>
2020-03-30 02:02:58 -0500

    fix

--- eventos/insert_externa.php
+++ eventos/insert_externa.php
@@ -4,21 +4,25 @@ $user = "trabajobd";
 $pass = "trabajobd";
 $DB = "trabajobd";
 $conn = mysqli_connect($host, $user, $pass, $DB) or die("Error al conectar a la DB " . mysqli_error($link));
+$nombre=$_POST[`nombre`];
 
-$query="INSERT INTO `empresa`(`nit`,`nombre`,`direccion`,`telefono`, `tipo`)
- 		VALUES ('$_POST[nit]','$_POST[nombre]','$_POST[direccion]','$_POST[telefono]','externa')";
-$result = mysqli_query($conn, $query) or die(mysqli_error($conn));
-if($result){
-	$query="INSERT INTO `externa`(`nit`,`representante`)
- 			VALUES ('$_POST[nit]','$_POST[representante]')";
+if($nombre===""){
+	$query="INSERT INTO `evento`(`cod_evento`,`razon`,`fecha`,`presupuesto`, `nobre`)
+ 	VALUES ('$_POST[cod_evento]','$_POST[razon]','$_POST[fecha]','$_POST[presupuesto]',NULL)";
 	$result = mysqli_query($conn, $query) or die(mysqli_error($conn));
-	if($result){
-	    header ("Location: empresas.php");
 	}else{
-	 	echo "Ha ocurrido un error al crear la empresa";
+	$query="INSERT INTO `evento`(`cod_evento`,`razon`,`fecha`, `presuspuesto`,`nombre`)
+ 		VALUES ('$_POST[cod_evento]','$_POST[razon]','$_POST[fecha]','$_POST[presupuesto]','$_POST[nombre])";
+$result = mysqli_query($conn, $query) or die(mysqli_error($conn));
+	}
+if($result){
+	if($result){
+	    header ("Location: eventos.php");
+	}else{
+	 	echo "Ha ocurrido un error al crear el evento";
 	}
 }else{
- 	echo "Ha ocurrido un error al crear la empresa";
+ 	echo "Ha ocurrido un error al crear el evento";
 }
 
 
