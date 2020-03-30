6872c273fcaa1bb606d7582f70c26ebad18bcc99
Pai-san <62859618+Pai-san@users.noreply.github.com>
2020-03-29 22:56:09 -0500

    commit

--- empresas/select_empresas.php
+++ empresas/select_empresas.php
@@ -1,12 +1,16 @@
 ﻿<?php
  
 // Create connection
-require('../configuraciones/conexionbd.php');
+$host = "localhost";
+$user = "root";
+$pass = "root";
+$DB = "trabajobd";
+$conn = mysqli_connect($host, $user, $pass, $DB) or die("Error al conectar a la DB " . mysqli_error($link));;
 
 //query
-$query="SELECT * FROM (empresa natural join externa) natural join contratada;
+$query="SELECT *
+ FROM empresa AS e  inner join  contratada AS con ON e.nit=con.nit";
 $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
-
 mysqli_close($conn);
 
 ?>
